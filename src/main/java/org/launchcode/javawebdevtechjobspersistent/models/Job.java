31e0ec93d09ea64db6f4cb9529bfd9d06de32490
Lawbud <lbudwell@gmail.com>
2021-07-11 17:28:44 -0500

    2nd commit, 2nd part

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -1,6 +1,8 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Job{
@@ -9,6 +11,8 @@ public class Job{
     @GeneratedValue
     private int id;
 
+    @NotBlank
+    @Size(min = 3, max = 999)
     private String name;
 
     private String employer;
